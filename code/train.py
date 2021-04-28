48eaa4e73d8fe14de7248b2efe2e1f4607d057bb
Sharon Zhang <zsharonx@gmail.com>
2021-04-27 21:34:33 -0400

    refactor models into separate class

--- code/train.py
+++ code/train.py
@@ -3,72 +3,43 @@ from os import path, makedirs
 import numpy as np
 import torch
 import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.optim as optim
 import copy
 
+from classifier import Classifier
 from load_data import *
-
-def train_model(model, outdir, device, dataloaders, criterion, optimizer, num_epochs):
+    
+def train_model(classifier, outdir, device, dataloaders, criterion, optimizer, num_epochs):
     since = time.time()
 
-    val_acc_history, loss_epoch_list = [], []
+    val_acc_history = []; loss_epoch_list = []
 
-    best_model_wts = copy.deepcopy(model.state_dict())
+    best_model_wts = copy.deepcopy(classifier.model.state_dict())
     best_acc = 0.0
 
+    running_loss = 0.0; running_corrects = 0
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
         print('-' * 10)
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
+            running_loss = 0.0; running_corrects = 0
             if phase == 'train':
-                model.train()  # Set model to training mode
+                running_loss += classifier.train(dataloaders['train'], criterion)
             else:
-                model.eval()   # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for i, (inputs, labels, _) in enumerate(dataloaders[phase]):
-                start = time.time()
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    # Get model outputs and calculate loss
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
-                    # print(loss)
-                    _, preds = torch.max(outputs, 1)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-                end = time.time()
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-                print("Iter {} (Epoch {}), Train Loss = {:.3f} Time / Batch = {:.3f}".format(i, epoch, loss.item(), end - start))
+                running_corrects += classifier.test(dataloaders['val'], criterion)
 
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f} Acc: {:.4f}  Time: {:.4f}'.format(phase, epoch_loss, epoch_acc, time.time() - since))
 
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
+                best_model_wts = copy.deepcopy(classifier.model.state_dict())
 
                 # save model
                 torch.save(best_model_wts, 'model_{}.pth'.format(arg['model']))
@@ -78,21 +49,24 @@ def train_model(model, outdir, device, dataloaders, criterion, optimizer, num_ep
                 loss_epoch_list.append(epoch_loss)
 
                 # Save the model
-                if (epoch + 1) % 1 == 0:
-                    torch.save(model.state_dict(), '{}/model_{}.pth'.format(outdir, epoch))
+                if (epoch + 1) % 5 == 0:
+                    torch.save(classifier.model.state_dict(), '{}/model_{}.pth'.format(outdir, epoch))
+                    
+        classifier.epoch += 1
 
     print('Best model at {} with lowest val loss {}'.format(np.argmin(loss_epoch_list), np.min(loss_epoch_list)))
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print('Best val acc: {:4f}'.format(best_acc))
 
     # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model, val_acc_history
+    classifier.model.load_state_dict(best_model_wts)
+    return classifier, val_acc_history
 
+"""
 def init_model(num_classes, feature_extract):
     # Initialize these variables which will be set in this if statement. Each of these
-    #   variables is model specific.
+    # variables is model specific.
     model_ft = models.resnet50(pretrained=True)
     set_parameter_requires_grad(model_ft, feature_extract)
     num_ftrs = model_ft.fc.in_features
@@ -105,12 +79,13 @@ def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.requires_grad = False
+"""
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dataset', type=str)
-    parser.add_argument('--batch', type=int, default=32)
+    parser.add_argument('--batchsize', type=int, default=32)
     parser.add_argument('--image_path', type=str)
     parser.add_argument('--labels_train', type=str)
     parser.add_argument('--labels_val', type=str)
@@ -118,44 +93,51 @@ def main():
     parser.add_argument('--num_epochs', type=int)
     parser.add_argument('--lr', type=float, default = 0.001)
     parser.add_argument('--num_classes', type=int, default=2)
+    parser.add_argument('--model_path', type=str, default=None)
     parser.add_argument('--device', default=torch.device('cpu'))
+    parser.add_argument('--dtype', default=torch.float32)
     parser.add_argument('--outdir', type=str)
     arg = vars(parser.parse_args())
 
     # Initialize the model
-    feature_extract = False
-    model_ft, input_size = init_model(arg['num_classes'], feature_extract)
+    optimizer_ft = optim.Adam(params_to_update, lr=arg['lr'])
+    criterion = nn.CrossEntropyLoss()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    classifier = Classifier(device=device, dtype=arg['dtype'],
+                            num_classes=arg['num_classes'], 
+                            input_size=256, 
+                            modelpath=arg['modelpath'])
 
     # Load Dataset
-    trainset = create_dataset(arg['dataset'], arg['image_path'], arg['labels_train'], arg['batch'])
-    valset = create_dataset(arg['dataset'], arg['image_path'], arg['labels_val'], arg['batch'], train=False)
+    trainset = create_dataset(arg['dataset'], arg['image_path'], 
+                              arg['labels_train'], arg['batchsize'])
+    valset = create_dataset(arg['dataset'], arg['image_path'], 
+                            arg['labels_val'], arg['batchsize'], train=False)
 
     dataloaders_dict = {
         'train': trainset,
         'val': valset
     }
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model_ft = model_ft.to(device)
-
-    params_to_update = model_ft.parameters()
+    # Observe that all parameters are being optimized
+    params_to_update = classifier.model.parameters()
+    feature_extract = False
     if feature_extract:
         params_to_update = []
-        for name,param in model_ft.named_parameters():
+        for name,param in classifier.model.named_parameters():
             if param.requires_grad == True:
                 params_to_update.append(param)
-                print("\t",name)
+                print("\t", name)
     else:
-        for name,param in model_ft.named_parameters():
+        for name,param in classifier.model.named_parameters():
             if param.requires_grad == True:
-                print("\t",name)
-
-    # Observe that all parameters are being optimized
-    optimizer_ft = optim.Adam(params_to_update, lr=arg['lr'])
-    criterion = nn.CrossEntropyLoss()
-    print(arg['num_epochs'])
+                print("\t", name)
+    
     # Train and evaluate
-    model_ft, hist = train_model(model_ft, arg['outdir'], arg['device'], dataloaders_dict, criterion, optimizer_ft, num_epochs=arg['num_epochs'])
+    print('Training for {} epochs'.format(arg['num_epochs']))
+    best_classifier, hist = train_model(classifier, arg['outdir'], arg['device'],
+                                        dataloaders_dict, criterion, optimizer_ft,
+                                        num_epochs=arg['num_epochs'])
 
 if __name__ == "__main__":
     main()
