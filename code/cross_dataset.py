5910f8c9c1c71f98ae9a8095112f630d5a73736b
christopher ye <czye@princeton.edu>
2021-04-27 23:04:34 -0400

    fleiss-kappa calculation for gender

--- code/cross_dataset.py
+++ code/cross_dataset.py
@@ -2,6 +2,7 @@ import torch
 import time
 import tqdm
 import numpy as np
+from statsmodels.stats.inter_rater import fleiss_kappa
 
 from load_data import create_dataset
 from utils import update_progress
@@ -10,17 +11,17 @@ from utils import update_progress
 #     models - list of strings of model save files
 #     dataloaders - list of (dataset, image_path, labels, batch)
 
-def accuracy_generalization_matrix(model_names, datasets):
+def accuracy_generalization_matrix(model_names, datasets, device):
     num_models = len(model_names)
     num_datasets = len(datasets)
     result = np.zeros((num_models, num_datasets))
 
     dataloaders = [create_dataset(dataset[0], dataset[1], dataset[2], dataset[3], train=False) for dataset in datasets]
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
     for i, model_name in enumerate(model_names):
         model = torch.load(model_name)
+        model = model.to(device)
+
         model.eval()
         for j, (dataset, dataloader) in enumerate(zip(datasets, dataloaders)):
             start = time.time()
@@ -46,6 +47,61 @@ def accuracy_generalization_matrix(model_names, datasets):
 
             print(f'Time Elapsed: {time.time() - start:.0f}s, Accuracy: {accuracy:.2f}%')
 
+    print(f'---- Cross-Dataset Generalization ----')
+    print(result)
+
+    return result
+
+
+def compute_fleiss_kappa(num_categories, model_names, device, dataloader):
+    num_subjects = len(dataloader.dataset)
+    fleiss_input = np.zeros((num_subjects, num_categories))
+
+    for model_name in model_names:
+        model = torch.load(model_name)
+        model = model.to(device)
+        model.eval()
+        start = time.time()
+        
+        for i, (inputs, _) in enumerate(dataloader):
+            inputs = inputs.to(device)
+            with torch.no_grad():
+                outputs = model(inputs)
+                for j, output in enumerate(outputs):
+                    fleiss_input[i * len(outputs) + j][output] += 1
+                
+                if i % 100 == 0:
+                    update_progress(i / len(dataloader.dataset))
+
+        print(f'Time Elapsed: {time.time() - start:.0f}s, Accuracy: {accuracy:.2f}%')
+
+    return fleiss_kappa(fleiss_input)
+
+
+def fleiss_kappa_gender(model_names, gender, dataset, device, num_genders=2):
+    # WARNING: requires dataloader shuffle to be FALSE
+    dataloader = create_dataset(dataset[0], dataset[1], dataset[2], dataset[3], train=False, gender=gender)
+    return compute_fleiss_kappa(num_genders, model_names, device, dataloader)
+    
+
+
+def fleiss_kappa_race(model_names, race, dataset, device, num_races=4):
+    # WARNING: requires dataloader shuffle to be FALSE
+    dataloader = create_dataset(dataset[0], dataset[1], dataset[2], dataset[3], train=False, race=race)
+    return compute_fleiss_kappa(num_races, model_names, device, dataloader)
+
+
+def fleiss_kappa_gender_matrix(model_names, datasets, device):
+    genders = ['M', 'F']
+    result = np.zeros((2, len(datasets)))
+
+    for i, gender in enumerate(genders):
+        for j, dataset in enumerate(datasets):
+            result[i][j] = fleiss_kappa_gender(model_names, gender, dataset, device)
+
+    print('----- FLEISS KAPPA MATRIX -----')
+    print(result)
+
     return result
 
 
@@ -53,7 +109,11 @@ def main():
     model_names = []
     datasets = []
 
-    accuracy_generalization_matrix(model_names, datasets)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    accuracy_generalization_matrix(model_names, datasets, device)
+
+    fleiss_kappa_gender_matrix(model_names, datasets, device)
 
 if __name__ == '__main__':
     main()
--- code/load_data.py
+++ code/load_data.py
@@ -26,11 +26,23 @@ class Dataset(Dataset):
 
         return X, y, ID
 
-def create_dataset(dataset, img_path, labels_path, batch_size, train=True):
+
+def get_image_paths(img_path, img_keys, img_labels, race=None, gender=None):
+    if gender is not None and race is not None:
+        return [img_path + x for x in img_keys if img_labels[x]['gender'] == gender and img_labels[x]['race'] == race]
+    elif gender is not None:
+        return [img_path + x for x in img_keys if img_labels[x]['gender'] == gender]
+    elif race is not None:
+        return [img_path + x for x in img_keys if img_labels[x]['race'] == race]
+    else:
+        return [img_path + x for x in img_keys]
+
+
+def create_dataset(dataset, img_path, labels_path, batch_size, train=True, gender=None, race=None):
     img_labels = pickle.load(open(labels_path, 'rb'))
     img_keys = sorted(list(img_labels.keys()))
-    img_paths = [img_path + x for x in sorted(list(img_labels.keys()))]
-
+    img_paths = get_image_paths(img_path, img_keys, img_labels, race, gender)
+    
     normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     # NO TRANSFORMATIONS ATM
