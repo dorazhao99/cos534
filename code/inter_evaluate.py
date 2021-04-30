157ec8ae90052ca616b3990b67278185a86d1c79
Sharon Zhang <zsharonx@gmail.com>
2021-04-29 20:58:44 -0400

    add intersectional evaluation code

---
+++ code/inter_evaluate.py
@@ -0,0 +1,78 @@
+import pickle, time, argparse, json
+from os import path, mkdir
+import numpy as np
+import torch
+import torch.optim as optim
+from torchvision import models
+from sklearn.metrics import accuracy_score
+
+from classifier import Classifier
+from load_data import *
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modelpath_gender', type=str, default=None)
+    parser.add_argument('--modelpath_race', type=str, default=None)
+    parser.add_argument('--humanlabels_gender', type=str, default=None)
+    parser.add_argument('--humanlabels_race', type=str, default=None)
+    parser.add_argument('--labels_gender', type=str, default=None)
+    parser.add_argument('--labels_race', type=str, default=None)
+    parser.add_argument('--batchsize', type=int, default=170)
+    parser.add_argument('--device', default=torch.device('cuda'))
+    parser.add_argument('--dtype', default=torch.float32)
+    parser.add_argument('--num_classes', type=int, default=4)
+    parser.add_argument('--outfile', type=str)
+    arg = vars(parser.parse_args())
+    print(arg, '\n', flush=True)
+    
+    humanlabels_gender = json.load(open(arg['humanlabels_gender']))
+    humanlabels_race = json.load(open(arg['humanlabels_race']))
+    labels_gender = pickle.load(open(arg['labels_gender'], 'rb'))
+    labels_race = pickle.load(open(arg['labels_race'], 'rb'))
+
+    # Initialize the model
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    classifier_gender = Classifier(device=device, dtype=arg['dtype'],
+                                   num_classes=arg['num_classes'], 
+                                   input_size=256, 
+                                   modelpath=arg['modelpath_gender'])
+    classifier_race = Classifier(device=device, dtype=arg['dtype'],
+                                 num_classes=arg['num_classes'], 
+                                 input_size=256, 
+                                 modelpath=arg['modelpath_race'])
+    
+    # Create dataloader
+    testset_gender = create_dataset(labels_path=arg['labels_gender'], batch_size=arg['batchsize'], train=False)
+    testset_race = create_dataset(labels_path=arg['labels_race'], batch_size=arg['batchsize'], train=False)
+
+    # Do inference with the models
+    _, _, y_preds_gender, y_true_gender = classifier_gender.test(testset_gender)
+    y_preds_gender = torch.cat(y_preds_gender); y_true_gender = torch.cat(y_true_gender)
+    y_preds_gender = y_preds_gender.detach().cpu().numpy()
+    y_true_gender = y_true_gender.detach().cpu().numpy()
+    
+    _, _, y_preds_race, y_true_race = classifier_race.test(testset_race)
+    y_preds_race = torch.cat(y_preds_race); y_true_race = torch.cat(y_true_race)
+    y_preds_race = y_preds_race.detach().cpu().numpy()
+    y_true_race = y_true_race.detach().cpu().numpy()
+
+    # Print out total accuracy
+    corrects = torch.sum((y_preds_gender==y_true_gender) & (y_preds_race==y_true_race))
+    acc = 100 * corrects.double() / len(testset.dataset)
+    print("Total accuracy: {:.2f}%".format(acc), flush=True)
+
+    for g in humanlabels_gender:
+        for r in humanlabels_race:
+            y_preds = 10. * y_preds_gender + y_preds_race
+            y_true = 10. * y_true_gender + y_true_race
+            inter_acc = accuracy_score(y_true, y_pred)
+
+            print('{:>8}, {>:16}: {:.2f}%'.format(g, r, 100. * inter_acc), flush=True)
+
+    # Print results to file
+    #output = {'pred': list(y_preds.tolist()), 'true': np.stack(y_true).tolist(), 'labels': [x for x in labels]}
+    #with open(arg['outfile'], 'w') as f:
+    #    json.dump(output, f)
+    
+if __name__ == "__main__":
+    main()
