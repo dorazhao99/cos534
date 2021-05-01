8c75a09479dfea9985699affdbf3ed000eeba575
Sharon Zhang <zsharonx@gmail.com>
2021-05-01 15:59:25 -0400

    fix bugs

--- code/compute_fleiss_kappa.py
+++ code/compute_fleiss_kappa.py
@@ -6,23 +6,23 @@ import argparse
 
 from statsmodels.stats.inter_rater import fleiss_kappa
 
-def compute_fleiss_kappa(pred, true, group, num_categs):
+def compute_fleiss_kappa(eval_pred, eval_true, group, num_categs):
     """
-    pred: a list of all predictions for race or gender
-    true: a list of all ground truth labels for race or gender, in 
-          the same order as the pred list
+    eval_pred: a list of lists of all predictions for race or gender for each model
+    eval_true: a list of lists of all ground truth labels for race or gender, in 
+          the same order as the pred list, for each model
     group: an int describing the group (e.g. 0 for Female, 1 for Male, etc.) 
            to compute the Fleiss-Kappa score over
     num_categs: int for all possible category labels (e.g., 2 for gender, 4 for race)
     """
-
-    num_subjects = true.count(group)
+    num_subjects = len(np.where(np.array(eval_true)[0]==group)[0])
     fleiss_inputs = np.zeros((num_subjects, num_categs))
-    idx = 0
-    for i,pred in enumerate(pred):
-        if true[i] == group:
-            fleiss_inputs[idx][pred] += 1
-            idx += 1
+    for i,pred in enumerate(eval_pred):
+        idx = 0
+        for j,p in enumerate(pred):
+            if eval_true[i][j] == group:
+                fleiss_inputs[idx][p] += 1
+                idx += 1
     return fleiss_kappa(fleiss_inputs)
 
 parser = argparse.ArgumentParser()
@@ -41,30 +41,31 @@ race_to_idx = json.load(open(arg['race_to_idx']))
 eval = json.load(open(arg['evals'][0]))
 
 # Get all model predictions
-evals_pred = []
+evals_pred = []; evals_true = []
 for f in arg['evals']:
     eval = json.load(open(f))
     evals_pred.append(eval['pred'])
+    evals_true.append(eval['true'])
 
 # Compute gender fleiss kappas
-gender_pred = [p for p,_ in eval['pred']]
-gender_true = [t for t,_ in eval['true']]
+gender_pred = [[p for p,_ in eval] for eval in evals_pred]
+gender_true = [[t for t,_ in eval] for eval in evals_true]
 for g in gender_to_idx:
-    fleiss_kappa_score = compute_fleiss_kappa(gender_pred, gender_true, g, len(gender_to_idx))
+    fleiss_kappa_score = compute_fleiss_kappa(gender_pred, gender_true, gender_to_idx[g], len(gender_to_idx))
     print('{} Fleiss-K score: {:.2f}'.format(g, fleiss_kappa_score))
 
 # Compute race fleiss kappas
-race_pred = [p for _,p in eval['pred']]
-race_true = [t for _,t in eval['true']]
+race_pred = [[p for _,p in eval] for eval in evals_pred]
+race_true = [[t for _,t in eval] for eval in evals_true]
 for r in race_to_idx:
-    fleiss_kappa_score = compute_fleiss_kappa(race_pred, race_true, r, len(race_to_idx))
+    fleiss_kappa_score = compute_fleiss_kappa(race_pred, race_true, race_to_idx[r], len(race_to_idx))
     print('{} Fleiss-K score: {:.2f}'.format(r, fleiss_kappa_score))
 
 # Compute intersectional fleiss kappas
-inter_pred = [10 * gp + rp for gp,rp in eval['pred']]
-inter_true = [10 * gt + rt for gt,rt in eval['true']]
+inter_pred = [[len(race_to_idx) * gp + rp for gp,rp in eval] for eval in evals_pred]
+inter_true = [[len(race_to_idx) * gt + rt for gt,rt in eval] for eval in evals_true]
 for g in gender_to_idx:
     for r in race_to_idx:
-        inter_idx = 10 * g + r
+        inter_idx = len(race_to_idx) * gender_to_idx[g] + race_to_idx[r]
         fleiss_kappa_score = compute_fleiss_kappa(inter_pred, inter_true, inter_idx, len(race_to_idx) * len(gender_to_idx))
         print('{}, {} Fleiss-K score: {:.2f}'.format(g, r, fleiss_kappa_score))
